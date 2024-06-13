a8c9c4f8264b5ed16b1603be22085d9584010c9f
monkeyhack <monkeyhack@localhost.localdomain>
2024-06-13 03:08:39 -0400

    version 1.0.1

--- run.sh
+++ run.sh
@@ -25,6 +25,6 @@ fi
 
 # Compilar y ejecutar el programa
 cd  ./scripts
-gcc -o jarvis brain/brain.c int/chat1.c out/hola.c jarvis.c command/ping.c command/mkdir.c command/find.c int/mirar.c  brain/order_file.c brain/order_folder.c
+gcc -o jarvis $(find . -name '*.c')
 xterm -geometry 114x12+1230+0 -e 'rlwrap ./jarvis' 2>/dev/null & disown
 cd ..
